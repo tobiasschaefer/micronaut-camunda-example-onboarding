95c7187aca3eec6c2b8152da935231eaef7d655f
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2021-02-15 21:20:17 +0100

    CreateCustomer, ScoreCustomer, and ProcessInstanceCreator (Start app) * create CreateCustomer and ScoreCustomer * create ProcessInstanceCreator

---
+++ onboarding/src/main/java/onboarding/CreateCustomer.java
@@ -0,0 +1,19 @@
+package onboarding;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class CreateCustomer implements JavaDelegate {
+
+    private static final Logger log = LoggerFactory.getLogger(CreateCustomer.class);
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        log.warn("Creating customer {} with result {}", execution.getBusinessKey(), execution.getVariable("result"));
+    }
+}
---
+++ onboarding/src/main/java/onboarding/ScoreCustomer.java
@@ -0,0 +1,20 @@
+package onboarding;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class ScoreCustomer implements JavaDelegate {
+
+    private static final Logger log = LoggerFactory.getLogger(ScoreCustomer.class);
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        log.warn("Scoring {}...", execution.getBusinessKey());
+        execution.setVariable("result", (int)(Math.random()*100));
+    }
+}
