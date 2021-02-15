afef38d83520377d1b68493ea370f7a60ed85eb8
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2021-02-15 21:05:05 +0100

    Empty Onboarding Application with feature "camunda" * mn create-app --features=camunda onboarding

---
+++ onboarding/src/test/java/onboarding/OnboardingTest.java
@@ -0,0 +1,20 @@
+package onboarding;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+@MicronautTest
+class OnboardingTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
